c6a15c3caec78b9d70f71f96943ff7986317e815
Rahul Singh <91553428+Rahul-singh7906@users.noreply.github.com>
2025-09-27 18:07:09 +0530

    portfolio

---
+++ src/components/About/AboutCard.js
@@ -0,0 +1,44 @@
+import React from "react";
+import Card from "react-bootstrap/Card";
+import { ImPointRight } from "react-icons/im";
+
+function AboutCard() {
+  return (
+    <Card className="quote-card-view">
+      <Card.Body>
+        <blockquote className="blockquote mb-0">
+          <p style={{ textAlign: "justify" }}>
+            Hi Everyone, I am <span className="purple">Rahul Singh </span>
+            from <span className="purple"> Uttar Pradesh, India.</span>
+            <br />
+            I am currently employed as a software developer at Opusify IT Solutions.
+            <br />
+            I have completed B.Tech Computer Science With specialization in Machine Learning at GLA University,
+            Mathura.
+            <br />
+            <br />
+            Apart from coding, some other activities that I love to do!
+          </p>
+          <ul>
+            <li className="about-activity">
+              <ImPointRight /> Playing Games
+            </li>
+            <li className="about-activity">
+              <ImPointRight /> Gymming
+            </li>
+            <li className="about-activity">
+              <ImPointRight /> Travelling
+            </li>
+          </ul>
+
+          <p style={{ color: "rgb(155 126 172)" }}>
+            "Strive to build things that make a difference!"{" "}
+          </p>
+          <footer className="blockquote-footer">Rahul</footer>
+        </blockquote>
+      </Card.Body>
+    </Card>
+  );
+}
+
+export default AboutCard;
---
+++ src/components/Projects/Projects.js
@@ -0,0 +1,63 @@
+import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import ProjectCard from "./ProjectCards";
+import Particle from "../Particle";
+import leaf from "../../Assets/Projects/leaf.png";
+import emotion from "../../Assets/Projects/emotion.png";
+import editor from "../../Assets/Projects/codeEditor.png";
+import rpa from "../../Assets/Projects/rpa.png";
+import suicide from "../../Assets/Projects/suicide.png";
+import bitsOfCode from "../../Assets/Projects/blog.png";
+
+function Projects() {
+  return (
+    <Container fluid className="project-section">
+      <Particle />
+      <Container>
+        <h1 className="project-heading">
+          My Recent <strong className="purple">Works </strong>
+        </h1>
+        <p style={{ color: "white" }}>
+          Here are a few projects I've worked on recently.
+        </p>
+        <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={rpa}
+              isBlog={false}
+              title="Deltan RPA"
+              description="Developed an end-to-end desktop automation tool for the Japanese company DELTAN. I used PyAutoGUI and PyWinAuto to streamline the conversion of proprietary client .DCM files to .STL format via Meditlink software "  
+            />
+          </Col>
+
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={bitsOfCode}
+              isBlog={false}
+              title="Monitor APP RPA"
+              description="Engineered an intelligent file detection system to automatically identify and
+              process new patient data stored locally on PCs and  Implemented seamless two-way data communication with the client’s database,
+              ensuring real-time data synchronization and operational efficiency"  
+            />
+          </Col>
+
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={editor}
+              isBlog={false}
+              title="Hira"
+              description="Collaborated in a team of 5 to develop a Japanese image-selling site, Project
+              Hira. Utilized Quasar,Vue.js, and Firebase for the development and deployment of
+              the site Handled database queries and operations using Firebase, ensuring efficient
+              data management"           
+            />
+          </Col>
+
+          
+        </Row>
+      </Container>
+    </Container>
+  );
+}
+
+export default Projects;
